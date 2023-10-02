6d9ec707911193a866436646097d8b3107f3cf01
Sayam Sethi <sayam.sethi2@gmail.com>
2023-10-02 17:42:30 -0500

    added testcase docstring

--- ece306_autograder/autograder_tests/lab1.py
+++ ece306_autograder/autograder_tests/lab1.py
@@ -4,6 +4,7 @@ from gradescope_utils.autograder_utils.decorators import partial_credit, visibil
 
 class TestLab1(unittest.TestCase):
     def _get_score(self, file):
+        print(file)
         try:
             with open(file, 'r') as f:
                 lines = f.read()
@@ -18,11 +19,13 @@ class TestLab1(unittest.TestCase):
     @partial_credit(30)
     @visibility('visible')
     def public_test_case(self, set_score=None):
+        """Public test case"""
         set_score(self._get_score(
             '/autograder/submission/public.out') / 100 * 30)
 
     @partial_credit(50)
     @visibility('after_published')
     def private_test_case(self, set_score=None):
+        """Private test case"""
         set_score(self._get_score(
             '/autograder/submission/private.out') / 100 * 80)
